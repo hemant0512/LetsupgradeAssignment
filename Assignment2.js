ed643e1b88757bbad139ccdcc97f082b9a6d428d
hemant0512 <73020754+hemant0512@users.noreply.github.com>
2021-07-12 19:36:23 +0530

    Create Assignment2.js

---
+++ Assignment2.js
@@ -0,0 +1,37 @@
+//Palindrome Program
+
+s = "madam is my mom "
+s1 = "" 
+s2 = ""
+  for (t of s) {
+    if (t != " ") {
+      s2 = s2 + t
+      s1 = t + s1
+    }
+    else {
+      if (s1 == s2) console.log(s1)
+      s1 = ""
+      s2 = ""
+    }
+  }
+  
+// FIzz BUzz Program  
+
+n=100
+for(i=1;i<=n;i=i+1){
+ a= i/15;
+ b= i/5;
+ c= i/3;
+  if (Number.isInteger(a)) {
+    console.log("fizzbuzz")
+  }
+  else if (Number.isInteger(b)) {
+    console.log("buzz")
+  }
+  else if (Number.isInteger(c)) {
+    console.log("fizz")
+  }
+  else{
+    console.log(i)
+  }
+}
